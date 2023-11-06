3bae90af145ca223693dd45d4fd57eac378b63ce
gabri <gabrielrl2004@gmail.com>
2023-11-06 13:59:05 +0100

    Vistas main añadidas, controladores creados y carga correcta de login

--- src/main/java/com/cesur/aplicaciondual/controllers/LoginController.java
+++ src/main/java/com/cesur/aplicaciondual/controllers/LoginController.java
@@ -1,14 +1,23 @@
 package com.cesur.aplicaciondual.controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 
-public class LoginController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginController implements Initializable {
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
     }
 }
---
+++ src/main/java/com/cesur/aplicaciondual/controllers/MainViewAlumnoController.java
@@ -0,0 +1,14 @@
+package com.cesur.aplicaciondual.controllers;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainViewAlumnoController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
