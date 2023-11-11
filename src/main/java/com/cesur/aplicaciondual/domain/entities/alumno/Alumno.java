c279f7329be8019f2908be96b69e5011eb0feaf0
gabri <gabrielrl2004@gmail.com>
2023-11-11 15:39:17 +0100

    Implementacion de la clase Profesor y su relacion con alumno

--- src/main/java/com/cesur/aplicaciondual/domain/entities/alumno/Alumno.java
+++ src/main/java/com/cesur/aplicaciondual/domain/entities/alumno/Alumno.java
@@ -1,6 +1,16 @@
 package com.cesur.aplicaciondual.domain.entities.alumno;
 
+import com.cesur.aplicaciondual.domain.entities.profesor.Profesor;
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 
+@Entity
+@Table(name = "alumno")
 public class Alumno implements Serializable {
+    @Id
+    private String dni;
+    @ManyToOne
+    @JoinColumn(name = "id_tutor")
+    private Profesor tutor;
 }
--- src/main/java/com/cesur/aplicaciondual/domain/entities/profesor/Profesor.java
+++ src/main/java/com/cesur/aplicaciondual/domain/entities/profesor/Profesor.java
@@ -1,6 +1,72 @@
 package com.cesur.aplicaciondual.domain.entities.profesor;
 
-import java.io.Serializable;
+import com.cesur.aplicaciondual.domain.entities.alumno.Alumno;
+import jakarta.persistence.*;
 
+import java.io.Serializable;
+import java.util.List;
+
+@Entity
+@Table(name = "profesor")
 public class Profesor implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nombre;
+    private String apellidos;
+    private String email;
+    private String pass;
+
+    @OneToMany(mappedBy = "profesor")
+    private List<Alumno> alumnos;
+
+    public Profesor(){}
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public List<Alumno> getAlumnos() {
+        return alumnos;
+    }
+
+    public void setAlumnos(List<Alumno> alumnos) {
+        this.alumnos = alumnos;
+    }
 }
