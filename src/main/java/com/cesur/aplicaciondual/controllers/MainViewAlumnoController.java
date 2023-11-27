f0975f2c7ebf8dcd7f61f5f6417d4a16c5ad9153
Sxmue <samu_5@hotmail.es>
2023-11-27 21:14:06 +0100

    main-view-alumno diseño avanzado

--- src/main/java/com/cesur/aplicaciondual/controllers/MainViewAlumnoController.java
+++ src/main/java/com/cesur/aplicaciondual/controllers/MainViewAlumnoController.java
@@ -1,11 +1,26 @@
 package com.cesur.aplicaciondual.controllers;
 
 import javafx.fxml.Initializable;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainViewAlumnoController implements Initializable {
+
+    @javafx.fxml.FXML
+    private TextField txtFiltroNombreTarea;
+    @javafx.fxml.FXML
+    private RadioButton radioDual;
+    @javafx.fxml.FXML
+    private RadioButton RadioFCT;
+    @javafx.fxml.FXML
+    private DatePicker filtroFecha;
+    @javafx.fxml.FXML
+    private Label lblNombreTutor;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
