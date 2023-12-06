0ec5e29ce6c41518bb7e6c7d447bac818072f714
gabri <gabrielrl2004@gmail.com>
2023-12-06 20:01:49 +0100

    Avances en el diseño de la ventana de edicion y vista de alumno

--- src/main/java/com/cesur/aplicaciondual/controllers/EditAndShowAlumnoController.java
+++ src/main/java/com/cesur/aplicaciondual/controllers/EditAndShowAlumnoController.java
@@ -1,13 +1,20 @@
 package com.cesur.aplicaciondual.controllers;
 
 import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Circle;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class EditAndShowAlumnoController implements Initializable {
+    @javafx.fxml.FXML
+    private Circle marcoImagen;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        Image foto = new Image("img/alumno.jpg",false);
+        marcoImagen.setFill(new ImagePattern(foto));
     }
 }
