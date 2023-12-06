113c487fd9b6965ebe41ae25553465e4bbfd8e68
gabri <gabrielrl2004@gmail.com>
2023-12-06 17:16:33 +0100

    Import innecesarios quitados

--- src/main/java/com/cesur/aplicaciondual/controllers/LoginController.java
+++ src/main/java/com/cesur/aplicaciondual/controllers/LoginController.java
@@ -11,10 +11,6 @@ import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.stage.Stage;
-
 import java.net.URL;
 import java.util.Objects;
 import java.util.ResourceBundle;
