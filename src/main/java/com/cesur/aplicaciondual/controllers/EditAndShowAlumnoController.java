5226c47a7f42a2348e9cf530050801572b621e0b
gabri <gabrielrl2004@gmail.com>
2023-12-06 17:52:18 +0100

    Creacion de ventana de edicion y visualizacion de alumnos

---
+++ src/main/java/com/cesur/aplicaciondual/controllers/EditAndShowAlumnoController.java
@@ -0,0 +1,13 @@
+package com.cesur.aplicaciondual.controllers;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class EditAndShowAlumnoController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
