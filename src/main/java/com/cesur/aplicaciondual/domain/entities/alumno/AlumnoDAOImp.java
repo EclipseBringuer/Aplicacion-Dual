f1c426730cd3cc45136d2079ddf793f07bf5ea9e
gabri <gabrielrl2004@gmail.com>
2023-12-10 12:17:38 +0100

    Alumno DAO implements comentado con javadoc

--- src/main/java/com/cesur/aplicaciondual/domain/entities/alumno/AlumnoDAOImp.java
+++ src/main/java/com/cesur/aplicaciondual/domain/entities/alumno/AlumnoDAOImp.java
@@ -1,18 +1,24 @@
 package com.cesur.aplicaciondual.domain.entities.alumno;
 
 import com.cesur.aplicaciondual.domain.HibernateUtil;
-import com.cesur.aplicaciondual.domain.entities.empresa.Empresa;
-import com.cesur.aplicaciondual.domain.entities.profesor.Profesor;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 import org.slf4j.LoggerFactory;
 import java.util.ArrayList;
 
+/**
+ * Implementación de la interfaz AlumnoDAO que realiza operaciones CRUD para la entidad Alumno en la base de datos.
+ */
 public class AlumnoDAOImp implements AlumnoDAO{
 
     static final org.slf4j.Logger LOG = LoggerFactory.getLogger(AlumnoDAOImp.class);
 
+    /**
+     * Obtiene todos los alumnos de la base de datos (sin implementar actualmente).
+     *
+     * @return Lista de alumnos (actualmente nula).
+     */
     @Override
     public ArrayList<Alumno> getAll() {
         return null;
@@ -55,6 +61,12 @@ public class AlumnoDAOImp implements AlumnoDAO{
         return u;
     }
 
+    /**
+     * Guarda un nuevo alumno en la base de datos.
+     *
+     * @param data El alumno a guardar.
+     * @return El alumno guardado.
+     */
     @Override
     public Alumno save(Alumno data) {
         Alumno salida = null;
@@ -76,6 +88,11 @@ public class AlumnoDAOImp implements AlumnoDAO{
         return salida;
     }
 
+    /**
+     * Actualiza un alumno existente en la base de datos.
+     *
+     * @param data El alumno con los nuevos datos.
+     */
     @Override
     public void update(Alumno data) {
         try (Session s = HibernateUtil.getSessionFactory().openSession()) {
@@ -91,6 +108,11 @@ public class AlumnoDAOImp implements AlumnoDAO{
         }
     }
 
+    /**
+     * Elimina un alumno de la base de datos.
+     *
+     * @param data El alumno a eliminar.
+     */
     @Override
     public void delete(Alumno data) {
 
@@ -106,6 +128,13 @@ public class AlumnoDAOImp implements AlumnoDAO{
 
     }
 
+    /**
+     * Obtiene un alumno por su cuenta de usuario (email y contraseña).
+     *
+     * @param email La dirección de correo electrónico del alumno.
+     * @param pass  La contraseña del alumno.
+     * @return El alumno encontrado en la base de datos, o null si no se encuentra.
+     */
     public Alumno getByAccount(String email, String pass) {
         Alumno salida = null;
         try (Session s = HibernateUtil.getSessionFactory().openSession()) {
